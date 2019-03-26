6da686219959a17946cd581b5c07de201e1546cf
Petr Zahradnik <pezinek@gmail.com>
2019-03-26 17:04:37 +0100

    reverting back to 1.13.3

---
+++ build.sh
@@ -0,0 +1,61 @@
+export ESPURNA_FLAGS="-DUSE_PASSWORD=0"
+
+cd code
+
+#pio run -t upload -e itead-sonoff-basic
+
+# Office lamp
+#pio run -t upload -e magichome-led-controller-ota --upload-port 192.168.1.80
+
+# Sonoff RF Bridge
+#pio run -t upload -e itead-sonoff-rfbridge-ota --upload-port 192.168.1.86
+
+# Kitchen socket S20
+#pio run -t upload -e itead-s20-ota --upload-port 192.168.1.83
+
+# S26 socket
+#pio run -t upload -e itead-s20-ota --upload-port 192.168.1.89
+
+# Sonoff 4 - basement light
+#pio run -t upload -e itead-sonoff-basic-pir-ota --upload-port 192.168.1.81 
+
+# Sonoff 5
+pio run -t upload -e custom-ota --upload-port 192.168.1.82
+#pio run -t upload -e custom
+#ESPURNA_FLAGS="-DUSE_PASSWORD=0 -DUSE_CUSTOM_H" pio run -t upload -e itead-sonoff-basic
+#pio run -t upload -e itead-sonoff-basic-pir-ota --upload-port 192.168.1.82
+
+# leds8
+#pio run -t upload -e magichome-led-controller-ota --upload-port 192.168.1.91
+
+# leds9 - RIP
+#ESPURNA_FLAGS="$ESPURNA_FLAGS -DLIGHT_CH1_PIN=4 " \
+#pio run -t upload -e arilux-al-lc06-ota --upload-port 192.168.1.92
+#pio run -t upload -e arilux-al-lc06 #-ota --upload-port 192.168.4.1
+
+# leds10 - wrong FW - the board is likely arilux-al-lc03
+# but there isn't a config for this yet.
+# Red channel not working, no IR support
+#pio run -t upload -e arilux-al-lc06-ota --upload-port 192.168.1.93
+
+# Sonoff-Pow 
+#pio run -t upload -e itead-sonoff-pow-ota --upload-port 192.168.1.90
+
+# leds4
+#pio run -t upload -e magichome-led-controller-ota --upload-port 192.168.1.57
+
+# Sonoff 2
+#pio run -t upload -e itead-sonoff-basic-dht-ota --upload-port 192.168.1.59
+
+# Sonoff 6
+#pio run -t upload -e itead-sonoff-basic-ota --upload-port 192.168.1.96
+
+# Sonoff 7
+#pio run -t upload -e itead-sonoff-basic-ota --upload-port 192.168.1.97
+
+# Sonoff 8
+#pio run -t upload -e itead-sonoff-basic-ota --upload-port 192.168.1.98
+
+# Sonoff 9
+#pio run -t upload -e itead-sonoff-basic-ota --upload-port 192.168.1.99
+
---
+++ code/espurna/config/custom.h
@@ -0,0 +1,38 @@
+
+    // Info
+    #define MANUFACTURER        "ITEAD"
+    #define DEVICE              "SONOFF_BASIC_CUSTOM"
+
+    // Buttons
+    #define BUTTON1_PIN         0
+    #define BUTTON1_MODE        BUTTON_PUSHBUTTON | BUTTON_DEFAULT_HIGH
+    #define BUTTON1_RELAY       1
+
+    #define DUMMY_RELAY_COUNT   1
+
+    /*
+    #ifndef DIGITAL_SUPPORT
+    #define DIGITAL_SUPPORT      1
+    #endif
+    #define DIGITAL_PIN         14
+    #define DIGITAL_PIN_MODE    INPUT
+    #define DIGITAL_DEFAULT_STATE 0
+    */
+
+    #define BUTTON2_PIN         14
+    #define BUTTON2_MODE        BUTTON_PUSHBUTTON
+    #define BUTTON2_PRESS       BUTTON_MODE_ON
+    #define BUTTON2_CLICK       BUTTON_MODE_OFF
+    #define BUTTON2_DBLCLICK    BUTTON_MODE_OFF
+    #define BUTTON2_LNGCLICK    BUTTON_MODE_OFF
+    #define BUTTON2_LNGLNGCLICK BUTTON_MODE_OFF
+    #define BUTTON2_RELAY       02
+
+
+    // Relays
+    #define RELAY1_PIN          12
+    #define RELAY1_TYPE         RELAY_TYPE_NORMAL
+
+    // LEDs
+    #define LED1_PIN            13
+    #define LED1_PIN_INVERSE    1
