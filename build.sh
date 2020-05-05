addab89f10687a317064b42e260f46f3cd5046b7
Petr Zahradnik <pezinek@gmail.com>
2020-05-05 20:29:58 +0200

    build config for EMU

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@ export ESPURNA_FLAGS="-DUSE_PASSWORD=0"
 
 cd code
 
-#pio run -t upload -e itead-sonoff-basic
+#pio run -t upload -e itead-sonoff-basic-r2-dht 
+#pio run -t upload -e itead-sonoff-basic-ota --upload-port 192.168.1.103
 
 # Office lamp
 #pio run -t upload -e magichome-led-controller-ota --upload-port 192.168.1.80
@@ -10,8 +11,13 @@ cd code
 # Sonoff RF Bridge
 #pio run -t upload -e itead-sonoff-rfbridge-ota --upload-port 192.168.1.86
 
-# Kitchen socket S20
+# Kitchen socket S20 + PIR on GPIO1/TX
+#ESPURNA_FLAGS="$ESPURNA_FLAGS -DDEBUG_SERIAL_SUPPORT=0 -DBUTTON2_PIN=1 -DBUTTON2_MODE=0 -DBUTTON2_PRESS=2 -DBUTTON2_CLICK=3 -DBUTTON2_DBLCLICK=3 -DBUTTON2_LNGCLICK=3 -DBUTTON2_LNGLNGCLICK=3 -DBUTTON2_RELAY=2 -DRELAY2_PIN=5 -DRELAY2_TYPE=0" \
 #pio run -t upload -e itead-s20-ota --upload-port 192.168.1.83
+
+# Bathroom socket + PIR on GPIO3/RX
+#ESPURNA_FLAGS="$ESPURNA_FLAGS -DDEBUG_SERIAL_SUPPORT=0 -DBUTTON2_PIN=3 -DBUTTON2_MODE=0 -DBUTTON2_PRESS=2 -DBUTTON2_CLICK=3 -DBUTTON2_DBLCLICK=3 -DBUTTON2_LNGCLICK=3 -DBUTTON2_LNGLNGCLICK=3 -DBUTTON2_RELAY=2 -DRELAY2_PIN=5 -DRELAY2_TYPE=0" \
+#pio run -t upload -e itead-s20-ota --upload-port 192.168.1.104
 
 # S26 socket
 #pio run -t upload -e itead-s20-ota --upload-port 192.168.1.89
@@ -20,7 +26,7 @@ cd code
 #pio run -t upload -e itead-sonoff-basic-pir-ota --upload-port 192.168.1.81 
 
 # Sonoff 5
-pio run -t upload -e itead-sonoff-basic-pir 
+#pio run -t upload -e itead-sonoff-basic-pir 
 #pio run -t upload -e itead-sonoff-basic-pir-ota --upload-port 192.168.1.82
 
 # leds8
@@ -57,3 +63,11 @@ pio run -t upload -e itead-sonoff-basic-pir
 # Sonoff 9
 #pio run -t upload -e itead-sonoff-basic-ota --upload-port 192.168.1.99
 
+# Sonoff Touch
+#pio run -t upload -e itead-sonoff-touch-ota --upload-port /dev/ttyUSB0
+
+
+# EMU
+# RPN Rule to set: $temperature0 30 35 cmp3 1 + $relay1 $relay0 $relay1 or 1 3 index 0 relay
+ESPURNA_FLAGS="$ESPURNA_FLAGS -DDHT_SUPPORT=1 -DDHT_PIN=14 -DBUTTON2_MODE=7 -DBUTTON2_PIN=4 -DBUTTON2_RELAY=2 -DRELAY2_PIN=2 -DRPN_RULES_SUPPORT=1" \
+pio run -t upload -e wemos-d1mini-relayshield --upload-port 192.168.1.250 # /dev/ttyUSB0
